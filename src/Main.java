0c95b6116f6c774b67de9235089057a4fd0f0cbf
sikir <sikir2022@gmail.com>
2022-11-20 23:45:14 +0300

    Я успел!

---
+++ src/Main.java
@@ -0,0 +1,64 @@
+public class Main {
+    public static void main(String[] args) {
+       /* System.out.println("Комбинированное решение первых 2х задач 1го урока по массивам");*/
+    /*double[]COfRublishyFromProphBabaVanga={1.57, 7.654, 9.986, 0.01, 0.1, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01};*/
+   int NumMyProcessingDificultHW [] ={1,2,3};
+  /* int[] daysPerMounth=new int[12];
+    daysPerMounth[0]=31;
+    daysPerMounth[1]=28;
+    daysPerMounth[2]=31;
+    daysPerMounth[3]=30;
+    daysPerMounth[4]=31;
+    daysPerMounth[5]=30;
+    daysPerMounth[6]=31;
+    daysPerMounth[7]=31;
+    daysPerMounth[8]=30;
+    daysPerMounth[9]=31;
+    daysPerMounth[10]=30;
+    daysPerMounth[11]=31;
+    int januaryDays=daysPerMounth[0];
+    int februaryDays=daysPerMounth[1];
+    int marchDays=daysPerMounth[2];
+    int aprilDays=daysPerMounth[3];
+    int mayDays=daysPerMounth[4];
+    int juneDays=daysPerMounth[5];
+    int julyDays=daysPerMounth[6];
+    int augustDays=daysPerMounth[7];
+    int septemberDays=daysPerMounth[8];
+    int octoberDays=daysPerMounth[9];
+    int novemberDays=daysPerMounth[10];
+    int decemberDays=daysPerMounth[11];
+    /*for(int j=0;j<12;j++){
+        System.out.print(daysPerMounth[j]);
+    }*/
+
+    /*System.out.println("Дней в месяце- январь- "+januaryDays);
+    System.out.println("Дней в месяце-февраль - "+februaryDays);
+    System.out.println("Дней в месяце-март - "+marchDays);
+    System.out.println("Дней в месяце-апрель - "+aprilDays);
+    System.out.println("Дней в месяце-май - "+mayDays);
+    System.out.println("Дней в месяце-июнь - "+juneDays);
+    System.out.println("Дней в месяце-июль - "+julyDays);
+    System.out.println("Дней в месяце-август - "+augustDays);
+    System.out.println("Дней в месяце-сентябрь- "+septemberDays);
+    System.out.println("Дней в месяце-октябрь - "+octoberDays);
+    System.out.println("Дней в месяце- ноябрь- "+novemberDays);
+    System.out.println("Дней в месяце-декабрь - "+decemberDays);
+    System.out.println("Hello world!");*/
+    /*for(int x=0;x<3;x++){
+        System.out.print(NumMyProcessingDificultHW[x]);}*/
+    /*for (int y=0;y<12;y++){
+        System.out.println(COfRublishyFromProphBabaVanga[y]);}*/
+    /*System.out.println("Решение 3й задачи 1го урока");*/
+        /*for(int x=2;x>-1;x--){
+            System.out.print(NumMyProcessingDificultHW[x]);}*/
+        /*for(int y=11;y>=0;y--){System.out.print(" "+COfRublishyFromProphBabaVanga[y]+" ");}*/
+      /*for(int j=11;j>=0;j--){
+            System.out.print(" "+daysPerMounth[j]+" ");}*/
+      System.out.println("Решение 4й задачи");
+      for(int x=0;x<=1;x++){
+          x+=1;
+            System.out.print(NumMyProcessingDificultHW[x]);}
+    }
+}
+
